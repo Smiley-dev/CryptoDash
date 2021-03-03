990588805d6421c0e9b15bcec9134ab6af79cf91
SmileyDev <nemanja.gakovic@hotmail.com>
2021-03-03 17:17:25 +0100

    Add CoinGrid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "../Settings/ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function () {
   return (
     <Page name="settings">
       <WelcomeMessage />
       <ConfirmButton />
+      <CoinGrid />
     </Page>
   );
 }
