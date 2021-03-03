583ede1bea4f4b34966152c0bcfebfc32ebac52f
SmileyDev <nemanja.gakovic@hotmail.com>
2021-03-03 14:05:42 +0100

    Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 40px;
 `;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${(props) =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElem active={active}>{toPropperCase(name)}</ControlButtonElem>
+  );
+}
+
+function toPropperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
 
 export default function AppBar() {
   return (
     <Bar>
-      <div>CryptoDash</div>
+      <Logo>CryptoDash</Logo>
       <div></div>
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 }
