0f725796c05ef9651aa7e716a29052ba18865861
SmileyDev <nemanja.gakovic@hotmail.com>
2021-03-04 15:16:24 +0100

    Dashboard Grid

---
+++ src/Dashboard/PriceGrid.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+import PriceTile from "./PriceTile";
+
+const PriceGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
+  margin-top: 40px;
+`;
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return (
+    <AppContext.Consumer>
+      {({ prices }) => (
+        <PriceGrid>
+          {prices.map((price, index) => (
+            <PriceTile index={index} price={price} />
+          ))}
+        </PriceGrid>
+      )}
+    </AppContext.Consumer>
+  );
+}
---
+++ src/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Page from "../Shared/Page";
+import PriceGrid from "./PriceGrid";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function () {
+  return (
+    <Page name="dashboard">
+      <PriceGrid></PriceGrid>
+    </Page>
+  );
+}
