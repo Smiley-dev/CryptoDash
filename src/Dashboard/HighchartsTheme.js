7afe031df76290a98fa3d36b80463423e92ce2ab
SmileyDev <nemanja.gakovic@hotmail.com>
2021-03-05 13:51:54 +0100

    Add Light Theme

--- src/Dashboard/HighchartsTheme.js
+++ src/Dashboard/HighchartsTheme.js
@@ -1,3 +1,5 @@
+import { lightTheme } from "../Shared/Styles";
+
 const theme = {
   colors: [
     "#DDDF0D",
@@ -13,11 +15,11 @@ const theme = {
     "#aaeeee",
   ],
   chart: {
-    backgroundColor: "#061a44",
+    backgroundColor: lightTheme ? "white" : "#061a44",
     borderColor: "#000000",
     borderWidth: 0,
     className: "dark-container",
-    plotBackgroundColor: "#061a44",
+    plotBackgroundColor: lightTheme ? "white" : "#061a44",
     plotBorderWidth: 0,
   },
   title: {
